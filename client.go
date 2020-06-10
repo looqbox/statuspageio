5023e804916af33f2648a82c5f7e5daee0191a5d
Nivaldo Melo <nivaldogmelo@gmail.com>
2020-06-10 17:22:08 -0300

    Change package structure

--- client.go
+++ client.go
@@ -7,29 +7,29 @@ import ()
 // BaseRequest struct store values used for every request at Statuspage.io API,
 // like the base url and the authorization header
 type BaseRequest struct {
-	Url     string
-	Headers []Header
+        Url     string
+        Headers []Header
 }
 
 // Header struct save information in Header format
 type Header struct {
-	Name  string
-	Value string
+        Name  string
+        Value string
 }
 
 // Connect mounts a base request to connect with the Statuspage.io API.
 // It needs a pageId and a apiKey that you can retrieve at the statuspage
 // manage page.
 func Connect(pageId string, apiKey string) BaseRequest {
-	request := BaseRequest{
-		Url: "https://api.statuspage.io/v1/pages/" + pageId,
-		Headers: []Header{
-			Header{
-				Name:  "Authorization",
-				Value: "OAuth " + apiKey,
-			},
-		},
-	}
+        request := BaseRequest{
+                Url: "https://api.statuspage.io/v1/pages/" + pageId,
+                Headers: []Header{
+                        Header{
+                                Name:  "Authorization",
+                                Value: "OAuth " + apiKey,
+                        },
+                },
+        }
 
-	return request
+        return request
 }
