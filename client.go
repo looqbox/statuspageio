802cadd3178cc61ff581db4ff167f6a5454005d2
Nivaldo Melo <nivaldogmelo@gmail.com>
2020-06-10 17:35:23 -0300

    Add documentation to package

--- client.go
+++ client.go
@@ -1,9 +1,15 @@
+// Package create to interact with statuspage.io API, created mainly to
+// interact with incidents creation/updating.
 package statuspageio
 
 import (
         "github.com/looqbox/statuspage-go/internal/root"
 )
 
+
+// Connect mounts a base request to connect with the Statuspage.io API.
+// It needs a pageId and a apiKey that you can retrieve at the statuspage
+// manage page.
 func Connect(pageId string, apiKey string) root.BaseRequest {
         request := root.BaseRequest{
                 Url: "https://api.statuspage.io/v1/pages/" + pageId,
