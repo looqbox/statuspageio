8dd306d99359958e9b5e300f946b927b015c3fd6
Nivaldo Melo <nivaldogmelo@gmail.com>
2020-06-10 16:49:50 -0300

    Add root package

--- pkg/root/root.go
+++ pkg/root/root.go
@@ -1,5 +1,6 @@
 package root
 
+<<<<<<< HEAD
 import (
         "encoding/json"
 )
@@ -32,4 +33,14 @@ type Header struct {
 
 type Component struct {
         ComponentId string `json:"component_id"`
+=======
+type BaseRequest struct {
+	Url     string
+	Headers []Header
+}
+
+type Header struct {
+	Name  string
+	Value string
+>>>>>>> 7af2d93... Add root package
 }
