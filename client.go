81037f4dedc99af468a35a03fc3d4d99738ad16b
Nivaldo Melo <nivaldogmelo@gmail.com>
2020-06-11 16:28:54 -0300

    Apply gofmt and golint requested changes

--- client.go
+++ client.go
@@ -1,35 +1,35 @@
-// Package create to interact with statuspage.io API, created mainly to
-// generate requests to manage incidents creation/update.
+// Package statuspageio generate requests used to interact with statuspage.io API
+// , created mainly to generate requests to manage incidents creation/update.
 package statuspageio
 
 import ()
 
-// Basic struct to store values used for every request at Statuspage.io
-// API, like the base url and the authorization header
+// BaseRequest struct store values used for every request at Statuspage.io API,
+// like the base url and the authorization header
 type BaseRequest struct {
-        Url     string
-        Headers []Header
+	Url     string
+	Headers []Header
 }
 
-// Struct to save Headers
+// Header struct save information in Header format
 type Header struct {
-        Name  string
-        Value string
+	Name  string
+	Value string
 }
 
 // Connect mounts a base request to connect with the Statuspage.io API.
 // It needs a pageId and a apiKey that you can retrieve at the statuspage
 // manage page.
 func Connect(pageId string, apiKey string) BaseRequest {
-        request := BaseRequest{
-                Url: "https://api.statuspage.io/v1/pages/" + pageId,
-                Headers: []Header{
-                        Header{
-                                Name:  "Authorization",
-                                Value: "OAuth " + apiKey,
-                        },
-                },
-        }
+	request := BaseRequest{
+		Url: "https://api.statuspage.io/v1/pages/" + pageId,
+		Headers: []Header{
+			Header{
+				Name:  "Authorization",
+				Value: "OAuth " + apiKey,
+			},
+		},
+	}
 
-        return request
+	return request
 }
