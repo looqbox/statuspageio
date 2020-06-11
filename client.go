660fcaa40ac79aef90899f2f639b2857e6d3ac9c
Nivaldo Melo <nivaldogmelo@gmail.com>
2020-06-11 15:45:16 -0300

    Add basic structs to client file

--- client.go
+++ client.go
@@ -3,14 +3,25 @@
 package statuspageio
 
 import (
-        "github.com/looqbox/statuspage-go/internal/root"
 )
 
+// Basic struct to store values used for every request at Statuspage.io
+// API, like the base url and the authorization header
+type BaseRequest struct {
+        Url     string
+        Headers []Header
+}
+
+// Struct to save Headers
+type Header struct {
+        Name  string
+        Value string
+}
 
 // Connect mounts a base request to connect with the Statuspage.io API.
 // It needs a pageId and a apiKey that you can retrieve at the statuspage
 // manage page.
-func Connect(pageId string, apiKey string) root.BaseRequest {
+func Connect(pageId string, apiKey string) BaseRequest {
         request := root.BaseRequest{
                 Url: "https://api.statuspage.io/v1/pages/" + pageId,
                 Headers: []root.Header{
