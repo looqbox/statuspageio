242436f0774a6e566aaad0b7c2a97c71633ef5d9
Nivaldo Melo <nivaldogmelo@gmail.com>
2020-06-11 15:45:16 -0300

    Add basic structs to client file

--- client_test.go
+++ client_test.go
@@ -4,18 +4,16 @@ import (
         "testing"
 
         "github.com/stretchr/testify/assert"
-
-        "github.com/looqbox/statuspage-go/internal/root"
 )
 
 func TestConnect(t *testing.T) {
         pageId := "wiV2d9pz8gdq0xAkNycXVcIEweV8KLw4"
         apiKey := "AsiSTLKioeurneEkdF41Q285y4d5I1sr"
 
-        expectedResult := root.BaseRequest{
+        expectedResult := BaseRequest{
                 Url: "https://api.statuspage.io/v1/pages/" + pageId,
-                Headers: []root.Header{
-                        root.Header{
+                Headers: []Header{
+                        Header{
                                 Name:  "Authorization",
                                 Value: "OAuth " + apiKey,
                         },
