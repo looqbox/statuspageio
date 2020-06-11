406a6be8d52b4dbe9bfd1990f69d02816cc3600b
Nivaldo Melo <nivaldogmelo@gmail.com>
2020-06-11 11:31:33 -0300

    Update documentation files

--- doc.go
+++
@@ -1,7 +0,0 @@
-//package statuspageio // import "github.com/looqbox/statuspage-go"
-//
-//Package create to interact with statuspage.io API, created mainly to
-//interact with incidents creation/updating.
-//
-//func Connect(pageId string, apiKey string) root.BaseRequest
-package statuspageio
