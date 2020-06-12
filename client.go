cfb9aa6619d2cafd22b911fa79fc30f4422f780c
Nivaldo Melo <nivaldogmelo@gmail.com>
2020-06-12 14:54:14 -0300

    Add Get, List and Update Incidents functions
    Apply go fmt to client files

--- client.go
+++ client.go
@@ -3,41 +3,41 @@
 package statuspageio
 
 import (
-        "net/http"
-        "time"
+	"net/http"
+	"time"
 )
 
 // BaseRequest struct store values used for every request at Statuspage.io API,
 // like the base url and the authorization header, also includes a http.Client
 // to execute requests
 type BaseRequest struct {
-        Url     string
-        Headers []Header
-        Client  *http.Client
+	Url     string
+	Headers []Header
+	Client  *http.Client
 }
 
 // Header struct save information in Header format
 type Header struct {
-        Name  string
-        Value string
+	Name  string
+	Value string
 }
 
 // Connect mounts a base request to connect with the Statuspage.io API.
 // It needs a pageId and a apiKey that you can retrieve at the statuspage
 // manage page.
 func Connect(pageId string, apiKey string) BaseRequest {
-        request := BaseRequest{
-                Url: "https://api.statuspage.io/v1/pages/" + pageId,
-                Headers: []Header{
-                        Header{
-                                Name:  "Authorization",
-                                Value: "OAuth " + apiKey,
-                        },
-                },
-                Client: &http.Client{
-                        Timeout: 30 * time.Second,
-                },
-        }
+	request := BaseRequest{
+		Url: "https://api.statuspage.io/v1/pages/" + pageId,
+		Headers: []Header{
+			Header{
+				Name:  "Authorization",
+				Value: "OAuth " + apiKey,
+			},
+		},
+		Client: &http.Client{
+			Timeout: 30 * time.Second,
+		},
+	}
 
-        return request
+	return request
 }
