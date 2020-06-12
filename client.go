2b781f9313e05750d32f2161a885de62d60b2847
Nivaldo Melo <nivaldogmelo@gmail.com>
2020-06-12 13:03:50 -0300

    Add new client structure along with docs and tests

--- client.go
+++ client.go
@@ -2,34 +2,42 @@
 // , created mainly to generate requests to manage incidents creation/update.
 package statuspageio
 
-import ()
+import (
+        "net/http"
+        "time"
+)
 
 // BaseRequest struct store values used for every request at Statuspage.io API,
-// like the base url and the authorization header
+// like the base url and the authorization header, also includes a http.Client
+// to execute requests
 type BaseRequest struct {
-	Url     string
-	Headers []Header
+        Url     string
+        Headers []Header
+        Client  *http.Client
 }
 
 // Header struct save information in Header format
 type Header struct {
-	Name  string
-	Value string
+        Name  string
+        Value string
 }
 
 // Connect mounts a base request to connect with the Statuspage.io API.
 // It needs a pageId and a apiKey that you can retrieve at the statuspage
 // manage page.
 func Connect(pageId string, apiKey string) BaseRequest {
-	request := BaseRequest{
-		Url: "https://api.statuspage.io/v1/pages/" + pageId,
-		Headers: []Header{
-			Header{
-				Name:  "Authorization",
-				Value: "OAuth " + apiKey,
-			},
-		},
-	}
+        request := BaseRequest{
+                Url: "https://api.statuspage.io/v1/pages/" + pageId,
+                Headers: []Header{
+                        Header{
+                                Name:  "Authorization",
+                                Value: "OAuth " + apiKey,
+                        },
+                },
+                Client: &http.Client{
+                        Timeout: 30 * time.Second,
+                },
+        }
 
-	return request
+        return request
 }
--- client_test.go
+++ client_test.go
@@ -1,26 +1,31 @@
 package statuspageio
 
 import (
-	"testing"
+        "testing"
+        "net/http"
+        "time"
 
-	"github.com/stretchr/testify/assert"
+        "github.com/stretchr/testify/assert"
 )
 
 func TestConnect(t *testing.T) {
-	pageId := "wiV2d9pz8gdq0xAkNycXVcIEweV8KLw4"
-	apiKey := "AsiSTLKioeurneEkdF41Q285y4d5I1sr"
+        pageId := "wiV2d9pz8gdq0xAkNycXVcIEweV8KLw4"
+        apiKey := "AsiSTLKioeurneEkdF41Q285y4d5I1sr"
 
-	expectedResult := BaseRequest{
-		Url: "https://api.statuspage.io/v1/pages/" + pageId,
-		Headers: []Header{
-			Header{
-				Name:  "Authorization",
-				Value: "OAuth " + apiKey,
-			},
-		},
-	}
+        expectedResult := BaseRequest{
+                Url: "https://api.statuspage.io/v1/pages/" + pageId,
+                Headers: []Header{
+                        Header{
+                                Name:  "Authorization",
+                                Value: "OAuth " + apiKey,
+                        },
+                },
+                Client: &http.Client{
+                        Timeout: 30 * time.Second,
+                },
+        }
 
-	result := Connect(pageId, apiKey)
+        result := Connect(pageId, apiKey)
 
-	assert.Equal(t, expectedResult, result)
+        assert.Equal(t, expectedResult, result)
 }
