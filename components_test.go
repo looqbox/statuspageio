99267d565e56d2d1af5f8d931924cccf4be94a6a
Nivaldo Melo <nivaldogmelo@gmail.com>
2020-06-18 17:26:09 -0300

    Add components files
    Add ListComponents function to list components at status page, it
    comes along with tests and docs

---
+++ components_test.go
@@ -0,0 +1,29 @@
+package statuspageio
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	componentId = "PHrzHysLcKu0axEWLyg2tsaW7xKWwbRC"
+)
+
+func TestListComponents(t *testing.T) {
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "OAuth "+apiKey, r.Header.Get("Authorization"))
+		assert.Equal(t, "GET", r.Method)
+		w.Write([]byte(okResponse))
+	})
+	testServer := httptest.NewServer(h)
+
+	statuspage := Connect(pageId, apiKey)
+	statuspage.Url = "http://" + testServer.Listener.Addr().String()
+
+	status, _ := statuspage.ListComponents()
+
+	assert.Equal(t, "200 OK", status)
+}
