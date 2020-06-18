290b87ed67ec3ac4ad66635ef47ffc395d020f07
Nivaldo Melo <nivaldogmelo@gmail.com>
2020-06-18 17:26:09 -0300

    Add components files
    Add ListComponents function to list components at status page, it
    comes along with tests and docs

---
+++ components_structs.go
@@ -0,0 +1,22 @@
+package statuspageio
+
+import (
+	"time"
+)
+
+// ComponentsResponse stores response information for Components requests
+type ComponentsResponse []struct {
+	ID                 string    `json:"id"`
+	PageID             string    `json:"page_id"`
+	GroupID            string    `json:"group_id"`
+	CreatedAt          time.Time `json:"created_at"`
+	UpdatedAt          time.Time `json:"updated_at"`
+	Group              bool      `json:"group"`
+	Name               string    `json:"name"`
+	Description        string    `json:"description"`
+	Position           int       `json:"position"`
+	Status             string    `json:"status"`
+	Showcase           bool      `json:"showcase"`
+	OnlyShowIfDegraded bool      `json:"only_show_if_degraded"`
+	AutomationEmail    string    `json:"automation_email"`
+}
