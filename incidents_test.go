42a0cb6d5f28b49f1d4a1b62233e46bbac25570e
Nivaldo Melo <nivaldogmelo@gmail.com>
2020-06-23 17:04:58 -0300

    Fix listIncidents function

--- incidents_test.go
+++ incidents_test.go
@@ -1,122 +1,123 @@
 package statuspageio
 
 import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
-	"testing"
+        "log"
+        "bytes"
+        "encoding/json"
+        "io/ioutil"
+        "net/http"
+        "net/http/httptest"
+        "testing"
 
-	"github.com/stretchr/testify/assert"
+        "github.com/stretchr/testify/assert"
 )
 
 var exampleBody = IncidentBody{
-	Name:         "Test Incident",
-	Status:       "Investigating",
-	Impact:       "Minor",
-	Notification: false,
-	Body:         "Test Body",
-	Components: Component{
-		ComponentID: "nd83293he9hr3",
-	},
-	ComponentsIDs: []string{"nfoeriu038d", "fnwe8789f"},
+        Name:         "Test Incident",
+        Status:       "Investigating",
+        Impact:       "Minor",
+        Notification: false,
+        Body:         "Test Body",
+        Components: Component{
+                ComponentID: "nd83293he9hr3",
+        },
+        ComponentsIDs: []string{"nfoeriu038d", "fnwe8789f"},
 }
 
 const (
-	okResponse = `{
+        okResponse = `{
                 "Status": "ok"
         }`
 
-	pageID     = "wiV2d9pz8gdq0xAkNycXVcIEweV8KLw4"
-	apiKey     = "AsiSTLKioeurneEkdF41Q285y4d5I1sr"
-	incidentID = "PHrzHysLcKu0axEWLyg2tsaW7xKWwbRC"
+        pageID     = "wiV2d9pz8gdq0xAkNycXVcIEweV8KLw4"
+        apiKey     = "AsiSTLKioeurneEkdF41Q285y4d5I1sr"
+        incidentID = "PHrzHysLcKu0axEWLyg2tsaW7xKWwbRC"
 )
 
 func TestListIncidents(t *testing.T) {
-	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "OAuth "+apiKey, r.Header.Get("Authorization"))
-		assert.Equal(t, "GET", r.Method)
-		w.Write([]byte(okResponse))
-	})
-	testServer := httptest.NewServer(h)
+        h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+                assert.Equal(t, "OAuth "+apiKey, r.Header.Get("Authorization"))
+                assert.Equal(t, "GET", r.Method)
+                w.Write([]byte(okResponse))
+        })
+        testServer := httptest.NewServer(h)
 
-	statuspage := Connect(pageID, apiKey)
-	statuspage.URL = "http://" + testServer.Listener.Addr().String()
+        statuspage := Connect(pageID, apiKey)
+        statuspage.URL = "http://" + testServer.Listener.Addr().String()
 
-	status, _ := statuspage.ListIncidents("")
+        status, _ := statuspage.ListIncidents("")
 
-	assert.Equal(t, "200 OK", status)
+        assert.Equal(t, "200 OK", status)
 }
 
 func TestGetIncidents(t *testing.T) {
-	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "OAuth "+apiKey, r.Header.Get("Authorization"))
-		assert.Equal(t, "GET", r.Method)
-		w.Write([]byte(okResponse))
-	})
-	testServer := httptest.NewServer(h)
+        h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+                assert.Equal(t, "OAuth "+apiKey, r.Header.Get("Authorization"))
+                assert.Equal(t, "GET", r.Method)
+                w.Write([]byte(okResponse))
+        })
+        testServer := httptest.NewServer(h)
 
-	statuspage := Connect(pageID, apiKey)
-	statuspage.URL = "http://" + testServer.Listener.Addr().String()
+        statuspage := Connect(pageID, apiKey)
+        statuspage.URL = "http://" + testServer.Listener.Addr().String()
 
-	status, _ := statuspage.GetIncident("")
+        status, _ := statuspage.GetIncident("")
 
-	assert.Equal(t, "200 OK", status)
+        assert.Equal(t, "200 OK", status)
 }
 
 func TestUpdateIncident(t *testing.T) {
-	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "OAuth "+apiKey, r.Header.Get("Authorization"))
-		assert.Equal(t, "PATCH", r.Method)
-		var body IncidentBody
-		responseBody, _ := ioutil.ReadAll(r.Body)
-		json.NewDecoder(bytes.NewBuffer(responseBody)).Decode(&body)
-		assert.Equal(t, exampleBody, body)
-		w.Write([]byte(okResponse))
-	})
-	testServer := httptest.NewServer(h)
+        h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+                assert.Equal(t, "OAuth "+apiKey, r.Header.Get("Authorization"))
+                assert.Equal(t, "PATCH", r.Method)
+                var body IncidentBody
+                responseBody, _ := ioutil.ReadAll(r.Body)
+                json.NewDecoder(bytes.NewBuffer(responseBody)).Decode(&body)
+                assert.Equal(t, exampleBody, body)
+                w.Write([]byte(okResponse))
+        })
+        testServer := httptest.NewServer(h)
 
-	statuspage := Connect(pageID, apiKey)
-	statuspage.URL = "http://" + testServer.Listener.Addr().String()
+        statuspage := Connect(pageID, apiKey)
+        statuspage.URL = "http://" + testServer.Listener.Addr().String()
 
-	status, _ := statuspage.UpdateIncident(incidentID, exampleBody)
+        status, _ := statuspage.UpdateIncident(incidentID, exampleBody)
 
-	assert.Equal(t, "200 OK", status)
+        assert.Equal(t, "200 OK", status)
 }
 
 func TestCreateIncidents(t *testing.T) {
-	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "OAuth "+apiKey, r.Header.Get("Authorization"))
-		assert.Equal(t, "POST", r.Method)
-		var body IncidentBody
-		responseBody, _ := ioutil.ReadAll(r.Body)
-		json.NewDecoder(bytes.NewBuffer(responseBody)).Decode(&body)
-		assert.Equal(t, exampleBody, body)
-		w.Write([]byte(okResponse))
-	})
-	testServer := httptest.NewServer(h)
+        h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+                assert.Equal(t, "OAuth "+apiKey, r.Header.Get("Authorization"))
+                assert.Equal(t, "POST", r.Method)
+                var body IncidentBody
+                responseBody, _ := ioutil.ReadAll(r.Body)
+                json.NewDecoder(bytes.NewBuffer(responseBody)).Decode(&body)
+                assert.Equal(t, exampleBody, body)
+                w.Write([]byte(okResponse))
+        })
+        testServer := httptest.NewServer(h)
 
-	statuspage := Connect(pageID, apiKey)
-	statuspage.URL = "http://" + testServer.Listener.Addr().String()
+        statuspage := Connect(pageID, apiKey)
+        statuspage.URL = "http://" + testServer.Listener.Addr().String()
 
-	status, _ := statuspage.CreateIncident(exampleBody)
+        status, _ := statuspage.CreateIncident(exampleBody)
 
-	assert.Equal(t, "200 OK", status)
+        assert.Equal(t, "200 OK", status)
 }
 
 func TestDeleteIncident(t *testing.T) {
-	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, "OAuth "+apiKey, r.Header.Get("Authorization"))
-		assert.Equal(t, "DELETE", r.Method)
-		w.Write([]byte(okResponse))
-	})
-	testServer := httptest.NewServer(h)
+        h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+                assert.Equal(t, "OAuth "+apiKey, r.Header.Get("Authorization"))
+                assert.Equal(t, "DELETE", r.Method)
+                w.Write([]byte(okResponse))
+        })
+        testServer := httptest.NewServer(h)
 
-	statuspage := Connect(pageID, apiKey)
-	statuspage.URL = "http://" + testServer.Listener.Addr().String()
+        statuspage := Connect(pageID, apiKey)
+        statuspage.URL = "http://" + testServer.Listener.Addr().String()
 
-	status, _ := statuspage.DeleteIncident("")
+        status, _ := statuspage.DeleteIncident("")
 
-	assert.Equal(t, "200 OK", status)
+        assert.Equal(t, "200 OK", status)
 }
